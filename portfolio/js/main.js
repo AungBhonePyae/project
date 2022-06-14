d52418fa0b133ac46b81e69000549a2e148f738b
AungBhonePyae <aungbhonepyae181@example.com>
2022-06-14 10:07:47 +0630

    project setup

---
+++ portfolio/js/main.js
@@ -0,0 +1,37 @@
+const toggle = document.querySelector(".toggle");
+const navbar = document.querySelector(".navbar-menu");
+const icon = document.querySelector(".fa-solid");
+
+toggle.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    icon.classList.toggle("fa-xmark");
+})
+
+const nav = document.querySelector("nav");
+
+const up=document.querySelector("#up");
+window.addEventListener("scroll", function () {
+    // if (window.scrollY >= 650) {
+    //     nav.style.backgroundColor = "#ddd";
+    // } else {
+    //     nav.style.backgroundColor = "";
+
+    // }
+    // if(window.scrollY>=850){
+    //     up.classList.add("active");
+    // }else{
+    //     up.classList.remove("active");
+    // }
+
+    // if(this.window.scrollY>=850) up.classList.add('active');
+    // else up.classList.remove("active");
+
+    window.scrollY >=850 ? up.classList.add('active'):up.classList.remove('active');
+})
+
+up.onclick=()=>{
+    window.scrollTo({
+        behavior: 'smooth',
+        top:0,
+    })
+}
